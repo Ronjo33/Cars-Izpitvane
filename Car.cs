b2303aa493018fbb729b143e1d924899a9790cf2
Ronjo33 <zlatomir.stefanov480@edu1.pgzaimov.com>
2023-05-10 11:40:33 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_9d
+{
+    internal class Car
+    {
+        public string brand;
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value;}
+        }
+
+        public string model;
+        public string Model
+        {
+            get { return  model; }
+            set { model = value; }
+        }
+
+        public string registrnum;
+        public string Registrnum
+        {
+            get { return registrnum; }
+            set { registrnum = value; }
+        }
+
+        public int god;
+        public int God
+        {
+            get { return god; }
+            set { god = value; }
+        }
+
+        public Car(string brand, string model, string rigistrnum, int god) 
+        {
+            this.brand = brand;
+            this.model = model;
+            this.registrnum = rigistrnum;
+            this.god = god;
+        } 
+        //metodi
+        public void dvijenie()
+        {
+            Console.WriteLine($"{brand}, {model} brum-brum-brum");
+        }
+        public void ECOstatus()
+        {
+            if (god>2000)
+            {
+                Console.WriteLine("ECO");
+            }
+            else
+            {
+                Console.WriteLine("ne sum ECO pushi qko");
+            }
+
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{brand}, {model}, {registrnum}, {god}");
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_9d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Car> Cars = new List<Car>();
+            int numberOfsustezanie = 0;
+            Console.WriteLine("shte vuvejdash li kola:");
+            string command = "da";
+            
+            while (command != "stop")
+            {
+                Console.Write("Brand:");
+                string brand = Console.ReadLine();
+
+                Console.Write("Model:");
+                string model = Console.ReadLine();
+
+                Console.Write("Regisnum:");
+                string registnum = Console.ReadLine();
+
+                Console.Write("god");
+                int god = int.Parse(Console.ReadLine());
+
+                Car car = new Car(brand, model, registnum, god);
+                Cars.Add(car);
+
+                Console.Write("shte vuvejdash li kola, ako ne stop za krai:");
+                command = Console.ReadLine();
+
+                numberOfsustezanie++;
+            }
+            Console.WriteLine();
+            foreach ( Car Car in Cars)
+            {
+                Car.Info();
+                Car.dvijenie();
+                Car.ECOstatus();
+            }
+            Console.WriteLine();
+            Console.WriteLine($"sustezatelite v sustezanieto sa {numberOfsustezanie}");
+        }
+    }
+}
